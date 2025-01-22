a24b0ba45e3c7ae7bc89448d3963540b99da3a52
mfooksa <mfooksa@gmail.com>
2025-01-22 08:58:23 -0700

    Added settings page and OAuth denial error messaging.

--- src/app/integration-error/page.tsx
+++ src/app/integration-error/page.tsx
@@ -2,15 +2,22 @@
 import {Container, Stack} from '@mui/material';
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { useSearchParams } from 'next/navigation';
 
 export default function IntegrationError() {
+  const searchParams = useSearchParams();
+
+  let errorText="An error occurred. Please try again later."
+  if(searchParams.get('error')=='denial'){
+    errorText = "In order to use this app, you need to grant access to your Klaviyo account. Please reconnect and click Accept to grant access to Klaviyo."
+  } 
 
   return (
     <Container maxWidth="md">
       <Stack spacing={2}>
         <Box alignContent={"center"} sx={{paddingTop: 5}}>
-          <Typography variant={"h1"} color={"text.secondary"} sx={{fontWeight: 'bold'}}>
-            ERROR OCCURRED
+          <Typography variant={"h2"} color={"text.secondary"} sx={{fontWeight: 'bold'}}>
+            {errorText}
           </Typography>
         </Box>
       </Stack>
